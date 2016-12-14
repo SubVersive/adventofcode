63a1ee6b78722891f6ae1aea78404d2177a3bf8d
mko <maksim.kondratyuk@gmail.com>
2016-12-14 01:15:21 +0100

    Fix mocha runner. Solve part 2 of day.

--- 2015/day2/index.js
+++ 2015/day2/index.js
@@ -1,10 +1,27 @@
+const os = require('os');
+
 module.exports = {
   exec1,
   exec2
 }; 
 
 function exec1(input) {
-  return 1;
+  const parts = input.split(os.EOL);
+  return parts.reduce((prev, curr) => {
+    return prev + single(curr);
+  }, 0);
+}
+
+function single(str) {
+  const [ l, w, h ] = str.split('x');
+  const p1 = l*w;
+  const p2 = w*h;
+  const p3 = h*l;
+  let min = p1;
+  if (p2 < p1 || p3 < p1) {
+    min = p3 < p2 ? p3 : p2;
+  }
+  return 2*p1 + 2*p2 + 2*p3 + min;
 }
 
 function exec2(input) {
--- 2015/day2/spec.js
+++ 2015/day2/spec.js
@@ -1,10 +1,16 @@
+const os = require("os");
 const func = require('./');
 
 describe('Day 2 challange', () => {
   describe('Part 1', () => {
-    if('for box 1x1x1 should return 7', () => {
-      'a'.should.equal('s');
-      // func.exec1('1x1x1').should.equal(7);
+    it('for box 1x1x1) should return 43', () => {
+      const input = `1x1x10`;
+      func.exec1(input).should.equal(43);
+    });
+
+    it('for box 2x3x4 should return 58', () => {
+      const input = `2x3x4`;
+      func.exec1(input).should.equal(58);
     });
   });
 
