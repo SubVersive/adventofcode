b449c0e7e77250e86dbd4bc738604f0cb7308fea
Maksym Kondratiuk <Maksim.Kondratyuk@check24.de>
2018-12-03 18:37:48 +0100

    Clean up code

--- 2018/day1/index.js
+++ 2018/day1/index.js
@@ -6,23 +6,17 @@ module.exports = {
 }; 
 
 function exec1(input) {
-  console.log({
-    input
-  })
   let result = 0;
   if (!input) {
     return result;
   }
 
   const lines = input.split(os.EOL);
-
   lines.forEach(line => {
     if (line.startsWith('+')) {
       result += parseInt(line.substring(1));
-    } else if (line.startsWith('-')) {
-      result -= parseInt(line.substring(1));
     } else {
-      throw new Error(`OOps ${line}`);
+      result -= parseInt(line.substring(1));
     }
   });
   return result;
