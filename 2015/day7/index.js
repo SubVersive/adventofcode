6e9cf1aae0a9d55725bd25691b463fb65f6fc61c
Maksym Kondratiuk <Maksym.Kondratiuk@check24.de>
2020-07-14 21:16:53 +0200

    Implement 2015 day 7 part 2

--- 2015/day7/index.js
+++ 2015/day7/index.js
@@ -11,7 +11,17 @@ function exec1() {
   return set.getSignal('a');
 }
 
-function exec2() {}
+function exec2() {
+  const aSignal = exec1();
+  const set = new WireSet();
+  const lines = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8').split(os.EOL);
+  lines.forEach((line) => set.add(line));
+  set.set['b'] = {
+    hasValue: true,
+    value: aSignal,
+  };
+  return set.getSignal('a');
+}
 
 module.exports = {
   exec1,
